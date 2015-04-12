db99bec81f8a4726f0ce12025feb2e015f78aa67
Ajay <Ajay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-12 08:03:40 +0000

    Uploaded v2.1.0
    git-svn-id: https://plugins.svn.wordpress.org/top-10/tags/2.1.0@1133047 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/deprecated.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Deprecated functions from Top 10. You shouldn't
+ * use these functions and look for the alternatives instead. The functions will be
+ * removed in a later version.
+ *
+ * @package Top_Ten
+ */
+
+
+
+
+?>
